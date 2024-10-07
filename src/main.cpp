c3483b18b0495fde2974e843f7fb6721aee0b139
gremble0 <hermanstornes@gmail.com>
2024-10-07 15:10:38 +0200

    Add some .clang-format rules

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <QApplication>
 #include <QMainWindow>
 
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
-    
-    QMainWindow window;
-    window.setWindowTitle("AlzWrite - lose yourself in text editing");
-    window.resize(640, 480);
-    window.show();
-    
-    return app.exec();
+int main(int argc, char *argv[]) {
+  QApplication app(argc, argv);
+
+  QMainWindow window;
+  window.setWindowTitle("AlzWrite - lose yourself in text editing");
+  window.resize(640, 480);
+  window.show();
+
+  return app.exec();
 }
